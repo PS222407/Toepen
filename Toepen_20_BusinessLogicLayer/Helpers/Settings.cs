ff1aaa9121026c1f10a9ed52108dcb6527ec91ab
Jens Ramakers <jensramakers@gmail.com>
2023-12-19 09:44:27 +0100

    10 penalty points

--- Toepen_20_BusinessLogicLayer/Helpers/Settings.cs
+++ Toepen_20_BusinessLogicLayer/Helpers/Settings.cs
@@ -4,7 +4,7 @@ public class Settings
 {
     public static bool LaundryOpenCards;
 
-    public static int MaxPenaltyPoints = 2;
+    public static int MaxPenaltyPoints = 10;
 
     public static int MaxCardsPerHand = 4;
 
